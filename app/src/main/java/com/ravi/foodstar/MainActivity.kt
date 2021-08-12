cbe395ce0704af75dc86a061382ee60dc76dede9
ravimishra1017.rm <ravimishra1017.rm@gmail.com>
2021-08-12 23:46:58 +0530

    Initial commit, added navigation components

---
+++ app/src/main/java/com/ravi/foodstar/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ravi.foodstar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipeFragment,
+                R.id.favouriteFragment,
+                R.id.jokeFragment
+            )
+        )
+
+       // bottom.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
