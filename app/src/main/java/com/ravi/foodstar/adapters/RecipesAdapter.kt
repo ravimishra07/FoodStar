a845bba3371bc4b11e1fe3bc924d307180f06764
ravi.mishra1 <ravi.mishra1@auw.co.in>
2021-09-01 23:19:30 +0530

    added diff util for recipe row layout

---
+++ app/src/main/java/com/ravi/foodstar/adapters/RecipesAdapter.kt
@@ -0,0 +1,53 @@
+package com.ravi.foodstar.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.ravi.foodstar.databinding.RecipesRowLayoutBinding
+import com.ravi.foodstar.models.FoodRecipe
+import com.ravi.foodstar.models.Result
+import com.ravi.foodstar.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(result: Result){
+                binding.result = result
+                binding.executePendingBindings()
+            }
+
+        companion object{
+            fun from(parent: ViewGroup):MyViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return  MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+    fun setData(newData: FoodRecipe){
+        val recipeDiffUtil = RecipesDiffUtil(recipe,newData.results)
+        val diffUtilResult  = DiffUtil.calculateDiff(recipeDiffUtil)
+        recipe = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
